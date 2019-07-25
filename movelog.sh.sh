a9891a1ce462ed64516584eb038b0eee6269381a
Swetchha Shukla <53063567+Swetchha@users.noreply.github.com>
2019-07-25 19:05:51 -0400

    Update movelog.sh.sh

--- movelog.sh.sh
+++ movelog.sh.sh
@@ -2,5 +2,5 @@
 
 sleep 5
 find /tmp/state.* -type f -mtime +14 -exec rm -f {} \;
-cat /tmp/state.log >> /tmp/state.$(date +"%Y%m%d")
+cat /tmp/state.log >> /tmp/statelog.$(date +"%Y%m%d")
 rm /tmp/state.log
