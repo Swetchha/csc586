4f3aedf13dab394a6f478f3a86297512e192e1ff
Swu05 <53063567+Swetchha@users.noreply.github.com>
2019-07-26 23:26:44 -0400

    Update slapdAutomation.sh

--- slapdAutomation.sh
+++ slapdAutomation.sh
@@ -80,7 +80,7 @@ ldap-auth-config        ldap-auth-config/move-to-debconf        boolean true
 
 # Setup SSO on client
 sudo apt-get update
-sudo apt install -y libnss-ldap -y libpam-ldap ldap-utils
+sudo apt install -y libnss-ldapd -y libpam-ldap ldap-utils
 
 # Provide hostname of node in the ldap.conf file
 sudo sed -i 's|ldapi:///|ldap://192.168.1.1|g' /etc/ldap.conf
