f38981a37227d9aa7c5e0a8965ed31fffd76d323
Swu05 <53063567+Swetchha@users.noreply.github.com>
2019-07-27 21:22:48 -0400

    Update setupldap.sh

--- setupldap.sh
+++ setupldap.sh
@@ -32,7 +32,7 @@ sudo dpkg-reconfigure slapd
 # Enable firewall rule
 sudo ufw allow ldap
 
-#sudo apt install apache2
+sudo apt install apache2
 
 # Populate LDAP
 ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w password -f basedn.ldif
