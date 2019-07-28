75038b2e18557be919c9bbb2f6287ca464f3eed1
Swu05 <53063567+Swetchha@users.noreply.github.com>
2019-07-28 15:09:04 -0400

    Update setupldap.sh

--- setupldap.sh
+++ setupldap.sh
@@ -4,21 +4,22 @@
 export DEBIAN_FRONTEND=noninteractive
 
 echo -e " 
-slapd slapd/internal/generated_adminpw password password
-slapd slapd/internal/adminpw password password
-slapd slapd/password2 password password
 slapd slapd/password1 password password
-slapd slapd/root_password password password
-slapd slapd/root_password_again password password
-slapd slapd/domain string clemson.cloudlab.us
-slapd shared/organization string clemson.cloudlab.us
-slapd slapd/backend select MDB 
+slapd slapd/internal/adminpw password password
+slapd slapd/internal/generated_adminpw password password
+slapd slapd/password2 password password
+slapd slapd/unsafe_selfwrite_acl note
 slapd slapd/purge_database boolean false
-slapd slapd/move_old_database boolean true
-slapd slapd/dump_database select when needed
-slapd slapd/allow_ldap_v2 boolean false
-slapd slapd/no_configuration boolean false
+slapd slapd/domain string clemson.cloudlab.us
+slapd slapd/ppolicy_schema_needs_update select abort installation
 slapd slapd/invalid_config boolean true
+slapd slapd/move_old_database boolean false
+slapd slapd/backend select MDB
+slapd shared/organization string clemson.cloudlab.us
+slapd slapd/dump_database_destdir string /var/backups/slapd-VERSION
+slapd slapd/no_configuration boolean false
+slapd slapd/dump_database select when needed
+slapd slapd/password_mismatch note
 " | sudo debconf-set-selections
 
 sudo apt-get update
