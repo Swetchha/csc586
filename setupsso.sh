7df4d1c236ad8640645f075d616d1be06ae12df6
Swu05 <53063567+Swetchha@users.noreply.github.com>
2019-07-29 11:19:15 -0400

    Update setupsso.sh

--- setupsso.sh
+++ setupsso.sh
@@ -6,12 +6,11 @@ export DEBIAN_FRONTEND=noninteractive
 echo -e " 
 ldap-auth-config        ldap-auth-config/rootbindpw     password password
 ldap-auth-config        ldap-auth-config/bindpw password password
-ldap-auth-config        ldap-auth-config/binddn string  cn=proxyuser,dc=example,dc=net
+ldap-auth-config        ldap-auth-config/binddn string  cn=admin,dc=clemson,dc=cloudlab,dc=us
 ldap-auth-config        ldap-auth-config/override       boolean true
 ldap-auth-config        ldap-auth-config/pam_password   select  md5
 ldap-auth-config        ldap-auth-config/dblogin        boolean false
-slapd   slapd/allow_ldap_v2     boolean false
-libpam-runtime  libpam-runtime/profiles multiselect     unix, ldap, systemd, capability
+libpam-runtime          libpam-runtime/profiles multiselect     unix, ldap, systemd, capability
 ldap-auth-config        ldap-auth-config/ldapns/base-dn string  dc=clemson,dc=cloudlab,dc=us
 ldap-auth-config        ldap-auth-config/dbrootlogin    boolean true
 ldap-auth-config        ldap-auth-config/ldapns/ldap_version    select  3
