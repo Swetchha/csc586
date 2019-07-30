e8e542ebb808a8b68608cf54c06cc11a2127be58
Swu05 <53063567+Swetchha@users.noreply.github.com>
2019-07-30 16:47:07 -0400

    Update slapdAutomation.sh

--- slapdAutomation.sh
+++ slapdAutomation.sh
@@ -1,24 +1,28 @@
 #! /bin/bash
 
-# Install openLDAP server non-interactively
+# Install openLDAP server quietly
 export DEBIAN_FRONTEND=noninteractive
 
 echo -e " 
-slapd slapd/internal/generated_adminpw password password
-slapd slapd/internal/adminpw password password
-slapd slapd/password2 password password
 slapd slapd/password1 password password
+slapd slapd/internal/adminpw password password
+slapd slapd/internal/generated_adminpw password password
+slapd slapd/password2 password password
 slapd slapd/root_password password password
 slapd slapd/root_password_again password password
-slapd slapd/domain string clemson.cloudlab.us
-slapd shared/organization string clemson.cloudlab.us
-slapd slapd/backend select MDB 
+slapd slapd/unsafe_selfwrite_acl note
 slapd slapd/purge_database boolean false
-slapd slapd/move_old_database boolean true
-slapd slapd/dump_database select when needed
+slapd slapd/domain string clemson.cloudlab.us
+slapd slapd/ppolicy_schema_needs_update select abort installation
+slapd slapd/invalid_config boolean true
+slapd slapd/move_old_database boolean false
+slapd slapd/backend select MDB
+slapd shared/organization string clemson.cloudlab.us
+slapd slapd/dump_database_destdir string /var/backups/slapd-VERSION
 slapd slapd/allow_ldap_v2 boolean false
 slapd slapd/no_configuration boolean false
-slapd slapd/invalid_config boolean true
+slapd slapd/dump_database select when needed
+slapd slapd/password_mismatch note
 " | sudo debconf-set-selections
 
 sudo apt-get update
@@ -26,14 +30,14 @@ sudo apt-get update
 # Grab slapd and ldap-utils (pre-seeded)
 sudo apt-get install -y slapd ldap-utils
 
-# Must reconfigure slapd for it to work properly 
+# Reconfigure slapd for it to work properly 
 sudo dpkg-reconfigure slapd
 
-# Enable firewall rule
-sudo ufw allow ldap
+# Enable firewall rule 
+sudo ufw allow ldap 
 
 # Populate LDAP
-ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w "password" -f basedn.ldif
+ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w password -f basedn.ldif
 
 # Generate password hash
 PASS=$(slappasswd -s rammy)
@@ -56,20 +60,21 @@ homeDirectory: /home/student
 EOF
 
 # Populate LDAP
-ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w "password" -f users.ldif 
+ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w password -f users.ldif 
 
 # Test LDAP
 ldapsearch -x -LLL -b dc=clemson,dc=cloudlab,dc=us 'uid=student' cn gidNumber
 
+# Install SSO on client quietly
+export DEBIAN_FRONTEND=noninteractive
 echo -e " 
 ldap-auth-config        ldap-auth-config/rootbindpw     password password
 ldap-auth-config        ldap-auth-config/bindpw password password
-ldap-auth-config        ldap-auth-config/binddn string  cn=proxyuser,dc=example,dc=net
+ldap-auth-config        ldap-auth-config/binddn string  cn=admin,dc=clemson,dc=cloudlab,dc=us
 ldap-auth-config        ldap-auth-config/override       boolean true
 ldap-auth-config        ldap-auth-config/pam_password   select  md5
 ldap-auth-config        ldap-auth-config/dblogin        boolean false
-slapd   slapd/allow_ldap_v2     boolean false
-libpam-runtime  libpam-runtime/profiles multiselect     unix, ldap, systemd, capability
+libpam-runtime          libpam-runtime/profiles multiselect     unix, ldap, systemd, capability
 ldap-auth-config        ldap-auth-config/ldapns/base-dn string  dc=clemson,dc=cloudlab,dc=us
 ldap-auth-config        ldap-auth-config/dbrootlogin    boolean true
 ldap-auth-config        ldap-auth-config/ldapns/ldap_version    select  3
@@ -78,12 +83,10 @@ ldap-auth-config        ldap-auth-config/ldapns/ldap-server     string  ldap://1
 ldap-auth-config        ldap-auth-config/move-to-debconf        boolean true
 " | sudo debconf-set-selections
 
-# Setup SSO on client
 sudo apt-get update
-sudo apt install -y libnss-ldapd -y libpam-ldap ldap-utils
 
-# Provide hostname of node in the ldap.conf file
-sudo sed -i 's|ldapi:///|ldap://192.168.1.1|g' /etc/ldap.conf
+# Grab libnss-ldap, libpam-ldap and ldap-utils (pre-seeded)
+sudo apt install -y libnss-ldap libpam-ldap ldap-utils
 
 # Enable LDAP profile for NSS
 sudo sed -i 's/systemd/systemd ldap/g' /etc/nsswitch.conf
